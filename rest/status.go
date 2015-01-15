19d70e174555224d37e814e47f9903c8ef428986
javinc <javinczki02@gmail.com>
2015-01-15 04:38:37 +0000

    add rest sample

---
+++ rest/status.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "github.com/ant0ine/go-json-rest/rest"
+    "log"
+    "net/http"
+)
+
+func main() {
+    handler := rest.ResourceHandler{
+        EnableStatusService: true,
+    }
+    err := handler.SetRoutes(
+        &rest.Route{"GET", "/.status",
+            func(w rest.ResponseWriter, r *rest.Request) {
+                w.WriteJson(handler.GetStatus())
+            },
+        },
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Fatal(http.ListenAndServe(":8080", &handler))
+}
